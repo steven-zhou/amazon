fc60545ade42c83f277f1d5773c0de652de01143
Anthony Winning <anthony@powernet.com.au>
2009-08-06 17:12:31 +1000

    Added a new function to return all the different model types in Amazon Settings.

--- app/models/amazon_setting.rb
+++ app/models/amazon_setting.rb
@@ -1,4 +1,17 @@
 class AmazonSetting < ActiveRecord::Base
   
 
+  def self.array_for_select
+    sql = "SELECT distinct(type) FROM amazon_settings"
+    db_data = ActiveRecord::Base.connection.execute(sql)
+
+    results = Array.new
+
+    for row in db_data do
+      results << [ ["#{row['type']}"], ["#{row['type']}"] ]
+    end
+
+    return results
+  end
+
 end
