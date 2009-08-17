b646c5a585bbc138ece7b7e19189ee08f2d3c1df
Anthony Winning <anthony@powernet.com.au>
2009-08-17 13:25:59 +1000

    Added some more tests for tag stuff.

--- app/models/tag_type.rb
+++ app/models/tag_type.rb
@@ -3,8 +3,8 @@ class TagType < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => [:type]
 
-  def distinct_types_of_tag_types
-    @tag_types = AmazonSetting.find(:all, :select => "DISTINCT type")
+  def self.distinct_types_of_tag_types
+    @tag_types = TagType.find(:all, :select => "DISTINCT type")
     results = ""
     @tag_types.each { |setting| results += "<option value='" + "#{setting.type}" + "'>" + "#{setting.type}" + "</option>" }
     return results
