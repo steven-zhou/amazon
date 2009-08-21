4d3f4ce50a3a5f83a31fc0cc4d04780ce5afb42e
Anthony Winning <anthony@powernet.com.au>
2009-08-21 11:58:32 +1000

    Finished RSpec tests for login control.

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -1,9 +1,7 @@
 Factory.define :role, :class => Role do |f|
   f.sequence(:name) { |n| "Role_#{n}" }
   f.sequence(:description) { |n| "description_#{n}" }
-  f.status true
+  f.role_status true
   f.sequence(:remarks) { |n| "remarks_#{n}" }
-
   f.association :role_type, :factory => :role_type
-  f.association :role_condition, :factory => :role_condition
 end
