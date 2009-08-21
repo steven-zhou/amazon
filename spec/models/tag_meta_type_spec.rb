68583fe5a9a83301887786598a946e64aeeba5ac
Wei <wei@powernet.com.au>
2009-08-21 16:33:07 +1000

    finish tag setting in admin

--- spec/models/tag_meta_type_spec.rb
+++ spec/models/tag_meta_type_spec.rb
@@ -12,14 +12,4 @@ describe TagMetaType do
     TagMetaType.distinct_types_of_tag_meta_types.should == "<option value='MasterDocMetaMetaType'>MasterDocMetaMetaType</option>"
   end
 
-  it "should change all children's status to be inactive if its status is change to be inactive" do
-    @tag_type = Factory(:doc_tag_type, :status => true)
-    @tag_meta_type.status = false
-    @tag_type.tag_meta_type = @tag_meta_type
-    @tag_meta_type.tag_types << @tag_type
-    @tag_meta_type.save
-
-    @tag_meta_type.tag_types[0].status.should == false
-  end
-
 end
--- spec/models/tag_type_spec.rb
+++ spec/models/tag_type_spec.rb
@@ -13,15 +13,5 @@ describe TagType do
     TagType.distinct_types_of_tag_types.should == "<option value='MasterDocMetaType'>MasterDocMetaType</option>"
   end
 
-  it "should change all children's status to be inactive if its status is change to be inactive" do
-    @tag = Factory(:doc_tag, :status => true)
-    @tag_type.status = false
-    @tag.tag_type = @tag_type
-    @tag_type.tags << @tag
-    @tag_type.save
-
-    @tag_type.tags[0].status.should == false
-  end
-
 end
 
