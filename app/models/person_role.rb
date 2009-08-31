7b6d2725172c0ac6b3557a93ed0139be50ddd361
Anthony Winning <anthony@powernet.com.au>
2009-08-31 12:09:19 +1000

    Some small RSpec test fixes.

--- app/models/person_role.rb
+++ app/models/person_role.rb
@@ -11,4 +11,12 @@ class PersonRole < ActiveRecord::Base
   validates_presence_of :role_id
   validates_presence_of :person_id
 
+ validate :role_must_exist
+
+ def role_must_exist
+   errors.add(:role_id, "You must specify a role that exists.") if (role_id && Role.find_by_id(role_id).nil?)
+ end
+
+
+
 end
