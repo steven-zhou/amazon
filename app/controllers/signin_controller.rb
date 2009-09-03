d36cad3b926fc6a7a7ad4bf1e5d8b5e0ed69e8f6
Anthony Winning <anthony@powernet.com.au>
2009-09-03 15:59:36 +1000

    Added ip address, log in and log out time for Login Accounts.

--- app/controllers/signin_controller.rb
+++ app/controllers/signin_controller.rb
@@ -8,13 +8,16 @@ class SigninController < ApplicationController
 
     if request.post?
       begin
-        session[:user] = LoginAccount.authenticate(params[:user_name], params[:password]).id
+        login_account = LoginAccount.authenticate(params[:user_name], params[:password])
+        session[:user] = login_account.id   # This will throw an exception if we do not have a valid login_account due to log in failing
+        login_account.update_attributes(:last_ip_address => request.remote_ip, :last_login => Time.now())
         if (session[:intended_action] != nil && session[:intended_controller] != nil)
           redirect_to :action => session[:intended_action],  :controller => session[:intended_controller]
         else
           redirect_to welcome_url
         end
       rescue
+        # If we threw an exception for not loggin in ok we will send a warning to the end user
         flash.now[:warning] = flash_message(:type => "login_error")
       end
     end
@@ -22,6 +25,8 @@ class SigninController < ApplicationController
   
   # Logs a user out.
   def signout
+    login_account = LoginAccount.find_by_id(session[:user])
+    login_account.update_attributes(:last_logoff => Time.now()) unless login_account.nil?
     session[:user] = nil
     redirect_to login_url
   end
