24249fc4dd382147bf99bac2a398459381de89a7
Wei <wei@powernet.com.au>
2009-09-17 11:26:32 +1000

    fix bugs

--- app/controllers/query_headers_controller.rb
+++ app/controllers/query_headers_controller.rb
@@ -14,12 +14,33 @@ class QueryHeadersController < ApplicationController
 
   def update
     @query_header = QueryHeader.find(params[:id].to_i)
+    if (!@query_header.query_criterias.empty?)
+      if(params[:query_header][:name] == @query_header.name)
+        @query_header.update_attributes(params[:query_header])
+        @query_header.group = "save"
+        @query_header.status = true if @query_header.status.nil?
+        @query_header.save
+      else
+        @query_header_new = Query_header.new(params[:query_header])
+        @query_header_new.group = "save"
+        @query_header_new.status = true if @query_header.status.nil?
+        @query_header_new.save
+        @query_header.query_criterias.each do |i|
+          @query_criterias.new
+        end
+      end
+      
 
-    if (!@query_header.query_criterias.empty? && @query_header.update_attributes(params[:query_header]))
-      @query_header.group = "save"
-      @query_header.status = true if @query_header.status.nil?
-      @query_header.save
-      flash.now[:message] = flash_message(:type => "object_created_successfully", :object => "query")
+      
+      
+      if (params[:new])
+        flash.now[:message] = flash_message(:type => "object_created_successfully", :object => "query")
+      else        
+        flash.now[:message] = flash_message(:type => "object_updated_successfully", :object => "query")
+      end
+      @query_criteria = QueryCriteria.new
+      @query_seleciton = QuerySelection.new
+      @query_sorter = QuerySorter.new
     else
       flash.now[:error] = "save unsuccessfully"
     end
@@ -69,7 +90,30 @@ class QueryHeadersController < ApplicationController
   end
 
   def edit
-    @query_header = QueryHeader.find(params[:id].to_i)
+    @query_header_selected = QueryHeader.find(params[:id].to_i)
+    @query_header = QueryHeader.new(@query_header_selected.attributes)
+    @query_header.name = @query_header_selected.name + " (Temp #{QueryHeader.random_name})"
+    @query_header.group = nil
+    @query_header.save
+
+    @query_header_selected.query_criterias.each do |i|
+      @query_criteria = QueryCriteria.new(i.attributes)
+      @query_criteria.query_header = @query_header
+      @query_criteria.save
+    end
+
+    @query_header_selected.query_selections.each do |i|
+      @query_selection = QuerySelection.new(i.attributes)
+      @query_selection.query_header = @query_header
+      @query_selection.save
+    end
+
+    @query_header_selected.query_sorters.each do |i|
+      @query_sorters = QuerySorter.new(i.attributes)
+      @query_sorters.query_header = @query_header
+      @query_sorters.save
+    end
+
     @query_criteria = QueryCriteria.new
     @query_seleciton = QuerySelection.new
     @query_sorter = QuerySorter.new
