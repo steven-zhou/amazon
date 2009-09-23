d90051c58a3086fed9535e51d19d56d90d20c402
lei <Lei@.powernet.com.au>
2009-09-23 18:12:06 +1000

    fix the bug of group_list

--- app/controllers/administrations_controller.rb
+++ app/controllers/administrations_controller.rb
@@ -14,6 +14,9 @@ class AdministrationsController < ApplicationController
     @login_account = LoginAccount.new
     @login_accounts = LoginAccount.find(:all)rescue @login_accounts = LoginAccount.new
     @user_group = UserGroup.new
+    @group_all = Array.new
+    c = GroupMetaType.find(:first, :conditions => ["name=?","System Users"])rescue @group_all = Array.new
+    @group_all = c.group_types
     respond_to do |format|
       format.html
     end
