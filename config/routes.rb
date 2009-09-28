badac5f15a8520551dcf071c58ad81cde7052338
Wei <wei@powernet.com.au>
2009-09-28 18:39:09 +1000

    fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -78,6 +78,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :group_lists, :collection => {:show_lists => :get}
 
+  map.resources :duplication_formulas
   map.resources :duplication_formula_details
   
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ spec/factories/duplication_formulas.rb
@@ -0,0 +1,4 @@
+Factory.define :duplication_formula do |f|
+  f.duplication_space "Primary List"
+  f.description "Duplication Formula"
+end
