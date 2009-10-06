4b23b60f647d160132203a97bd8a561ba64c2354
Anthony Winning <anthony@powernet.com.au>
2009-10-06 11:48:42 +1100

    Fixed a bug in the postcode assistant.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -42,7 +42,6 @@ class AddressesController < ApplicationController
   end
 
   def search_postcodes
-    puts "***** DEBUG Search postcodes is being run!"
     @postcodes = DomesticPostcode.find(:all, :conditions => ["suburb ILIKE ? AND state ILIKE ? AND postcode LIKE ?", "%#{params[:suburb]}%", "%#{params[:state]}%", "%#{params[:postcode]}%"])
     respond_to do |format|
       format.js
