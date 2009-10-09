94f4ae72a3773aab791fa9b59c708da98ca158ff
Wei <wei@powernet.com.au>
2009-10-09 16:00:12 +1100

    solve jquery grid problem

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -962,29 +962,23 @@ $(function(){
 });
 
 
-
-$(function() {
-    $(".password").jpassword(
-
-    {
-            lang: {
-                please: "please type password over 6 characters",
-                low: "Low security.",
-                correct: "Correct security.",
-                high: "High security.",
-                length: "-X- characters would be a plus.",
-                number: "Why not numbers?",
-                uppercase: "And caps?",
-                lowercase: "Some tiny?",
-                punctuation: "Punctuations?",
-                special: "Best, special characters?"
-            }
-        },
-        {
-            length: 6
-        }
-        );
-});
+//$(function() {
+//    $(".password").jpassword({
+//            lang: {
+//                please: "please type password over 6 characters",
+//                low: "Low security.",
+//                correct: "Correct security.",
+//                high: "High security.",
+//                length: "-X- characters would be a plus.",
+//                number: "Why not numbers?",
+//                uppercase: "And caps?",
+//                lowercase: "Some tiny?",
+//                punctuation: "Punctuations?",
+//                special: "Best, special characters?"
+//            },
+//            length: 6
+//    });
+//});
 
 
 
