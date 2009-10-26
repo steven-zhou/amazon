e49933245795e6ef61b5339a28e24ab51cc15115
Wei <wei@powernet.com.au>
2009-10-26 15:18:28 +1100

    change status to be checkbox

--- app/controllers/administrations_controller.rb
+++ app/controllers/administrations_controller.rb
@@ -28,7 +28,7 @@ class AdministrationsController < ApplicationController
   end
 
   def query_tables
-    @tag_meta_types = TableMetaMetaType.find(:all)
+    @tag_meta_types = TableMetaMetaType.find(:all, :order => "name asc")
     @category = "Table"
     respond_to do |format|
       format.html
@@ -36,7 +36,7 @@ class AdministrationsController < ApplicationController
   end
 
   def master_docs
-    @tag_meta_types = MasterDocMetaMetaType.find(:all)
+    @tag_meta_types = MasterDocMetaMetaType.find(:all, :order => "name asc")
     @category = "MasterDoc"
     respond_to do |format|
       format.html
@@ -44,7 +44,7 @@ class AdministrationsController < ApplicationController
   end
 
   def role_conditions
-    @tag_meta_types = MasterDocMetaMetaType.find(:all)
+    @tag_meta_types = MasterDocMetaMetaType.find(:all, :order => "name asc")
     @category = "MasterDoc"
     respond_to do |format|
       format.html
@@ -52,7 +52,7 @@ class AdministrationsController < ApplicationController
   end
 
   def access_permissions
-    @tag_meta_types = SystemPermissionMetaMetaType.find(:all)
+    @tag_meta_types = SystemPermissionMetaMetaType.find(:all, :order => "name asc")
     @category = "SystemPermission"
     respond_to do |format|
       format.html
@@ -67,7 +67,7 @@ class AdministrationsController < ApplicationController
   end
 
   def contact_types
-    @tag_meta_types = ContactMetaMetaType.find(:all)
+    @tag_meta_types = ContactMetaMetaType.find(:all, :order => "name asc")
     @category = "Contact"
     respond_to do |format|
       format.html
