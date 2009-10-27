f595a90b49b371aa48e0351fb23e389d4ef55516
matthew <matthew@powernet.com>
2009-10-28 10:52:51 +1100

    fix the email regular expession

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1724,7 +1724,7 @@ $(function(){
 });
 
 check_email_field = function(){
-    _valid = /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/.test($('#email_value').val());
+    _valid = /^([^@\s]+)@((?:[-a-z0-9A-Z]+\.)+[a-zA-Z]{2,})$/.test($('#email_value').val());
     if($('#email_value').val()!=""){
         if((!_valid)){
             alert("This field should be am email !");
@@ -1735,7 +1735,7 @@ check_email_field = function(){
 }
 
 check_email_field_edit = function(){
-    _valid = /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/.test($('#email_value_edit').val());
+    _valid = /^([^@\s]+)@((?:[-a-z0-9A-Z]+\.)+[a-z]{2,})$/.test($('#email_value_edit').val());
     if($('#email_value_edit').val()!=""){
         if((!_valid)){
             alert("This field should be am email !");
