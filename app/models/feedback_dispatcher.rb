615048c51c3a166c9dff7cc38ac2dd93642be682
Anthony Winning <anthony@powernet.com.au>
2009-10-28 14:15:26 +1100

    Changed the from email address.

--- app/models/feedback_dispatcher.rb
+++ app/models/feedback_dispatcher.rb
@@ -3,7 +3,7 @@ class FeedbackDispatcher < ActionMailer::Base
   def notify_admin(feedback)
 
     @recipients       = "magdy@powernet.com.au"
-    @from             = "Amazon System Feedback <noreply@powernet.com.au>"
+    @from             = "noreply@powernet.com.au"
     headers           = {'Precedence' => 'bulk', 'List-Unsubscribe' => 'powernet@powernet.com.au'}
     @subject          = "System Feedback"
     @sent_on          = Time.now
@@ -16,7 +16,7 @@ class FeedbackDispatcher < ActionMailer::Base
   def confirmation_email(feedback)
 
     @recipients       = "#{feedback.login_account.security_email}"
-    @from             = "Amazon System Feedback <noreply@powernet.com.au>"
+    @from             = "noreply@powernet.com.au"
     headers           = {'Precedence' => 'bulk', 'List-Unsubscribe' => 'powernet@powernet.com.au'}
     @subject          = "System Feedback Ticket #{feedback.id}"
     @sent_on          = Time.now
--- app/models/login_account_password_reset_dispatcher.rb
+++ app/models/login_account_password_reset_dispatcher.rb
@@ -3,7 +3,7 @@ class LoginAccountPasswordResetDispatcher < ActionMailer::Base
   def email_notification(login_account, new_password)
 
     @recipients       = "#{login_account.security_email}"
-    @from             = "Amazon System <noreply@powernet.com.au>"
+    @from             = "noreply@powernet.com.au"
     headers           = {'Precedence' => 'bulk', 'List-Unsubscribe' => 'powernet@powernet.com.au'}
     @subject          = "Password Reset Request"
     @sent_on          = Time.now
