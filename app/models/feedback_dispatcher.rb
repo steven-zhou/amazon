a32bae487f19efc8471b28faf91bdc7d9d2c9e2a
Anthony Winning <anthony@powernet.com.au>
2009-10-28 14:33:52 +1100

    Changed the models for sending emails.

--- app/models/feedback_dispatcher.rb
+++ app/models/feedback_dispatcher.rb
@@ -2,25 +2,25 @@ class FeedbackDispatcher < ActionMailer::Base
 
   def notify_admin(feedback)
 
-    @recipients       = "magdy@powernet.com.au"
-    @from             = "noreply@powernet.com.au"
-    headers           = {'Precedence' => 'bulk', 'List-Unsubscribe' => 'powernet@powernet.com.au'}
-    @subject          = "System Feedback"
-    @sent_on          = Time.now
-    @body["feedback"] = feedback
-    content_type        "text/html"
+    recipients       "magdy@powernet.com.au"
+    from             "anthony@powernet.com.au"
+    headers          = {'Precedence' => 'bulk', 'List-Unsubscribe' => 'powernet@powernet.com.au'}
+    subject          "System Feedback"
+    sent_on           Time.now
+    body             :feedback => feedback
+    content_type     "text/html"
 
   end
 
 
   def confirmation_email(feedback)
 
-    @recipients       = "#{feedback.login_account.security_email}"
-    @from             = "noreply@powernet.com.au"
-    headers           = {'Precedence' => 'bulk', 'List-Unsubscribe' => 'powernet@powernet.com.au'}
-    @subject          = "System Feedback Ticket #{feedback.id}"
-    @sent_on          = Time.now
-    @body["feedback"] = feedback
+    recipients       "#{feedback.login_account.security_email}"
+    from             "anthony@powernet.com.au"
+    headers          = {'Precedence' => 'bulk', 'List-Unsubscribe' => 'powernet@powernet.com.au'}
+    subject          "System Feedback Ticket #{feedback.id}"
+    sent_on          Time.now
+    body             :feedback => feedback
     content_type        "text/html"
 
   end
