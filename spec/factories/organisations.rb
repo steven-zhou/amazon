fbdd3224e9371b7e9c10790ad82ea45dd28a02fb
Wei <wei@powernet.com.au>
2009-10-28 19:00:55 +1100

    finish client setup

--- spec/factories/organisations.rb
+++ spec/factories/organisations.rb
@@ -1,4 +1,4 @@
-Factory.define :google, :class => Organisation do |f|
+Factory.define :google, :class => ClientOrganisation do |f|
   f.sequence(:id) { |n| n }
   f.custom_id "GOGL"
   f.full_name "Google Enterprises"
@@ -23,6 +23,25 @@ Factory.define :google, :class => Organisation do |f|
   f.association :business_type, :factory => :high_tech
   f.association :business_category, :factory => :search_engine
   f.association :industry_sector, :factory => :telecommunications
+end
+
+Factory.define :CNN, :class => OtherOrganisation do |f|
+  f.sequence(:id) { |n| n }
+  f.custom_id "CNN"
+  f.full_name "CNN"
+  f.short_name "CNN"
+  f.trading_as "CNN"
+  f.registered_name "CNN Company Ltd"
+  f.registered_number "56843545421212"
+  f.registered_date Date.today()
+  f.tax_file_no "54787423435455"
+  f.legal_no_1 ""
+  f.legal_no_2 ""
+  f.number_of_full_time_employees 5000
+  f.number_of_part_time_employees 1
+  f.number_of_contractors 2
+  f.number_of_volunteers 3
+  f.number_of_other_workers 4
 end
 
 Factory.define :invalid_organisation, :class =>Organisation do |f|
