dbc8fe385d19e4b01153ea7e526b16425d507dd8
lei <Lei@.powernet.com.au>
2009-10-29 10:33:50 +1000

    finish the group permission

--- app/controllers/group_permissions_controller.rb
+++ app/controllers/group_permissions_controller.rb
@@ -13,6 +13,49 @@ class GroupPermissionsController < ApplicationController
     end
   end
 
+
+
+
+
+  #  def show_methods
+  #    @system_permission_meta_type = SystemPermissionMetaType.find(params[:controller_id])  #controller
+  #    @system_permission_types = @system_permission_meta_type.system_permission_types
+  #    #puts"---DEBUG991---#{@system_permission_types.to_yaml}"
+  #     respond_to do |format|
+  #      format.js
+  #    end
+  #
+  #  end
+
+
+
+
+  #  def new
+  #
+  #    @group_type = GroupType.find(params[:group_id])
+  #    @group_permission = GroupPermission.new
+  #    @group_permissions = @group_type.group_permissions
+  #    @module_all = Array.new
+  #    @module_all = SystemPermissionMetaMetaType.all
+  #
+  #    respond_to do |format|
+  #      format.js
+  #    end
+  #
+  #  end
+
+
+  #*********new design************
+  def edit
+    @group_type = GroupType.find(params[:data_id])
+    @group_permission = GroupPermission.new
+    @group_permissions =  @group_type.group_permissions
+    @module_all = SystemPermissionMetaMetaType.all
+    respond_to do |format|
+      format.js
+    end
+  end
+
   def show_module
     @group_type = GroupType.find(params[:group_id])
     @system_permission_meta_meta_type = SystemPermissionMetaMetaType.find(params[:system_permission_module_id])
@@ -32,61 +75,33 @@ class GroupPermissionsController < ApplicationController
     end
   end
 
-  #  def show_methods
-  #    @system_permission_meta_type = SystemPermissionMetaType.find(params[:controller_id])  #controller
-  #    @system_permission_types = @system_permission_meta_type.system_permission_types
-  #    #puts"---DEBUG991---#{@system_permission_types.to_yaml}"
-  #     respond_to do |format|
-  #      format.js
-  #    end
-  #
-  #  end
+  def destroy
+
+    @group_permission = GroupPermission.find(params[:id].to_i)
+    @group_permission.destroy
+    @group_types = GroupType.system_user_groups
+
+    respond_to do |format|
+      format.js
+    end
+  end
 
   def create
-
-    #    #puts"-----dbug-----#{params[:system_permission_meta_meta_type][:name].to_yaml}"
-    #
-    #    unless params[:module_ids].blank?
-    #      #puts"---DEBUG[111]---#{params[:module_ids][]}"
-    #      #puts"---DEBUG[222]-#{params[:module_ids]}"
-    #      @system_permission_meta_meta_type = SystemPermissionMetaMetaType.find(params[:module_id])
-    #      @system_permission_meta_meta_type.system_permission_meta_types.each do |controller|
-    #        #puts"-----dbug-----#{controller.to_yaml}"
-    #        controller.system_permission_types.each do |method|
-    #          @group_permission = GroupPermission.new(:user_group_id => params[:group_id], :system_permission_type_id => method.id)
-    #
-    #          @group_permission.save!
-    #        end
-    #      end
-    #
-    #    end
-
     if params[:module_ids].blank?
-
       unless params[:method_ids].blank?
         params[:method_ids].each do |method_id|
-          #        @system_permission_type = SystemPermissionType.find_all_by_id(method_id)
-          #         @system_permission_meta_type = SystemPermissionType.find_all_by_id(method_id)
           @group_permission = GroupPermission.new(:user_group_id => params[:group_id], :system_permission_type_id => method_id)
           if  @group_permission.save
             flash.now[:message]= "saved successfully"
           else
             flash.now[:error] = flash_message(:type => "field_missing", :field => "system_permission")if (!@group_permission.errors[:system_permission_type_id].nil? && @group_permission.errors.on(:system_permission_type_id).include?("can't be blank"))
             flash.now[:error] = flash_message(:type => "field_missing", :field => "group")if (!@group_permission.errors[:user_group_id].nil? && @group_permission.errors.on(:user_group_id).include?("can't be blank"))
-           
           end
-
         end
-
       else
         flash.now[:error]= flash_message(:type => "field_missing", :field => "permission")
-       
       end
-
     else
-    
-      # else
-
       @system_permission_meta_meta_type = SystemPermissionMetaMetaType.find(params[:module_id])
       @system_permission_meta_meta_type.system_permission_meta_types.each do |controller|
         #puts"-----dbug-----#{controller.to_yaml}"
@@ -114,32 +129,6 @@ class GroupPermissionsController < ApplicationController
     end
   end
 
-  def destroy
-
-    @group_permission = GroupPermission.find(params[:id].to_i)
-    @group_permission.destroy
-
-    respond_to do |format|
-      format.js
-    end
-  end
-
-  def new
-
-     @group_type = GroupType.find(params[:group_id])
-    @group_permission = GroupPermission.new
-    @group_permissions = @group_type.group_permissions
-    #puts"---DEBUG111---#{params[:group_id].to_yaml}"
-    @module_all = Array.new
-    @module_all = SystemPermissionMetaMetaType.all
-
-     respond_to do |format|
-      format.js
-    end
-
-  end
-
-  
 
   
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1976,129 +1976,27 @@ $(function(){
 });
 
 
-$(function(){
+//$(function(){
+//
+//    $("#system_permission_meta_meta_type_id").live('change',function(){
+//
+//        if ($(this).val()!= ""){
+//            $.ajax({
+//                type:"GET",
+//                url: "/group_permissions/show_module.js",
+//                data:"system_permission_module_id="+$(this).val() + '&group_id=' + $("#group_permission_user_group_id").val(),
+//                dataType:"script"
+//
+//            });
+//            $('#permission_form').show();
+//        }else{
+//
+//            $('#permission_form').hide();
+//        }
+//
+//    });
+//});
 
-    $("#system_permission_meta_meta_type_id").live('change',function(){
-
-        if ($(this).val()!= ""){
-            $.ajax({
-                type:"GET",
-                url: "/group_permissions/show_module.js",
-                data:"system_permission_module_id="+$(this).val() + '&group_id=' + $("#group_permission_user_group_id").val(),
-                dataType:"script"
-
-            });
-            $('#permission_form').show();
-        }else{
-
-            $('#permission_form').hide();
-        }
-
-    });
-});
-
-
-$(function(){
-    $(".group_permission_color").live('mouseover', function(){
-        $(this).animate({
-            color: "#FFFF00"
-        }, 300)
-    });
-});
-
-$(function(){
-    $(".group_permission_color").live('mouseout', function(){
-        $(this).animate({
-            color: "#F7F8E0"
-        }, 300)
-    });
-});
-
-/*use for click the module show the whole thing*/
-$(function(){
-    $("#show_controllers").live('click', function(){
-        $.ajax({
-            type:"GET",
-            url:"/group_permissions/show_controllers.js",
-            data:"main_module_id="+$(this).attr('main_module_id'),
-            dataType:"script"
-
-        });
-
-    });
-
-});
-
-$(function(){
-    $(".module_select_all").live('click', function(){
-        if ($(this).attr("checked") == true){
-            $('.controller_select_all').attr("checked", true);
-            $('.method_select_all').attr("checked", true);
-        }else{
-            $('.controller_select_all').attr("checked", false);
-            $('.method_select_all').attr("checked", false);
-        }
-
-    });
-});
-
-$(function(){
-    $("#new_group_permission").live('click', function(){
-        $(this).css("display", "none");
-        $(".group_permission_delete").css("display", "none");
-
-        $.ajax({
-            type: "GET",
-            url: "/group_permissions/new.js",
-            data:"group_id="+$(this).attr('group_id'),
-            dataType: "script"
-        });
-        $("#close_new_module").css("display", "");
-    });
-});
-
-
-/*when you slecet the controller , all the method will be on*/
-$(function(){
-    $('.controller_select_all').live('click', function(){
-        if ($(this).attr('checked') == true){
-
-
-            $('.method_select_all[controller_id = ' + $(this).val() +']').attr("checked", true);
-
-
-        }else{
-
-
-            $('.method_select_all[controller_id='+ $(this).val()+']').attr("checked", false);
-            $('.module_select_all').attr("checked", false);
-
-        }
-    });
-});
-
-
-
-$(function(){
-    $('.method_select_all').live('click', function(){
-        if ($(this).attr('checked') == false){
-            $('.module_select_all').attr("checked", false);
-            $('.controller_select_all[controller_id='+ $(this).attr("controller_id")+ ']').attr("checked", false);
-        }
-
-    });
-});
-
-$(function(){
-    $("#close_new_module").live('click', function(){
-        $(this).css("display", "none");
-     
-        $('#hide_module').html('');
-        $('#new_group_permission').css("display", "");
-        $(".group_permission_delete").css("display", "");
-      
-    });
-});
 
 
 
@@ -2762,21 +2660,21 @@ $(function(){
 
 });
 
-$(function(){
-    $('.user_name_to_person').live('change', function(){
-        if($(this).val()!= ""){
-            $.ajax({
-                type: "GET",
-                url: "/user_groups/user_name_to_person.js",
-                data: 'user_name='+$(this).val(),
-                dataType:"script"
-            });
-        }else{
-            $("#login_name_container_"+$(this).attr('login_account_id')).html("");
-        }
-       
-    });
-});
+//$(function(){
+//    $('.user_name_to_person').live('change', function(){
+//        if($(this).val()!= ""){
+//            $.ajax({
+//                type: "GET",
+//                url: "/user_groups/user_name_to_person.js",
+//                data: 'user_name='+$(this).val(),
+//                dataType:"script"
+//            });
+//        }else{
+//            $("#login_name_container_"+$(this).attr('login_account_id')).html("");
+//        }
+//
+//    });
+//});
 
 //
 //
@@ -3057,7 +2955,12 @@ $(function(){
     $('.close_logo').live('click', function(){
         $("#new_" + $(this).attr('field')+ "_form").toggle('blind');
         $("#" + $(this).attr('field')+ "_edit_container").html('');
-        $(".container_selected").removeClass("container_selected");
+        if ($(this).attr('change_color') == "false"){
+            
+        }else{
+            $(".container_selected").removeClass("container_selected");
+        }
+       
     });
 });
 
@@ -3268,7 +3171,133 @@ $(function(){
 
     });
 
+});
 
+/*new-design--group permission*/
+
+$(function(){
+
+    $(".show_module_form").live('change',function(){
+
+        if ($(this).val()!= ""){
+            $.ajax({
+                type:"GET",
+                url: "/group_permissions/show_module.js",
+                data:"system_permission_module_id="+$(this).val() + '&group_id=' + $("#group_permission_user_group_id").val(),
+                dataType:"script"
+
+            });
+            $('#permission_form').show();
+        }else{
+
+            $('#permission_form').hide();
+        }
+
+    });
+});
+
+
+
+$(function(){
+    $(".group_permission_color").live('mouseover', function(){
+        $(this).animate({
+            color: "#FFFF00"
+        }, 300)
+    });
+});
+
+$(function(){
+    $(".group_permission_color").live('mouseout', function(){
+        $(this).animate({
+            color: "#F7F8E0"
+        }, 300)
+    });
+});
+
+/*use for click the module show the whole thing*/
+$(function(){
+    $("#show_controllers").live('click', function(){
+        $.ajax({
+            type:"GET",
+            url:"/group_permissions/show_controllers.js",
+            data:"main_module_id="+$(this).attr('main_module_id'),
+            dataType:"script"
+
+        });
+
+    });
 
 });
+
+$(function(){
+    $(".module_select_all").live('click', function(){
+        if ($(this).attr("checked") == true){
+            $('.controller_select_all').attr("checked", true);
+            $('.method_select_all').attr("checked", true);
+        }else{
+            $('.controller_select_all').attr("checked", false);
+            $('.method_select_all').attr("checked", false);
+        }
+
+    });
+});
+
+$(function(){
+    $("#new_group_permission").live('click', function(){
+        $(this).css("display", "none");
+        $(".group_permission_delete").css("display", "none");
+
+        $.ajax({
+            type: "GET",
+            url: "/group_permissions/new.js",
+            data:"group_id="+$(this).attr('group_id'),
+            dataType: "script"
+        });
+        $("#close_new_module").css("display", "");
+    });
+});
+
+
+/*when you slecet the controller , all the method will be on*/
+$(function(){
+    $('.controller_select_all').live('click', function(){
+        if ($(this).attr('checked') == true){
+
+
+            $('.method_select_all[controller_id = ' + $(this).val() +']').attr("checked", true);
+
+
+        }else{
+
+
+            $('.method_select_all[controller_id='+ $(this).val()+']').attr("checked", false);
+            $('.module_select_all').attr("checked", false);
+
+        }
+    });
+});
+
+
+
+$(function(){
+    $('.method_select_all').live('click', function(){
+        if ($(this).attr('checked') == false){
+            $('.module_select_all').attr("checked", false);
+            $('.controller_select_all[controller_id='+ $(this).attr("controller_id")+ ']').attr("checked", false);
+        }
+
+    });
+});
+
+$(function(){
+    $("#close_new_module").live('click', function(){
+        $(this).css("display", "none");
+
+        $('#hide_module').html('');
+        $('#new_group_permission').css("display", "");
+        $(".group_permission_delete").css("display", "");
+
+    });
+});
+
 
