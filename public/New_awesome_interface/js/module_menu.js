e4023e7fd5097d56cad5334592246eb8fcd23b96
Wei <wei@powernet.com.au>
2009-11-02 13:03:19 +1100

    add tree links to tag

--- public/New_awesome_interface/js/module_menu.js
+++ public/New_awesome_interface/js/module_menu.js
@@ -1,15 +1,21 @@
 // JavaScript Document
 $(document).ready(function() {
-						   $("#module_menu_top").click(function() {								
-								if($("div#module_menu_items").is(":hidden")) 
-									$("div#module_menu_top").addClass("hover"),
-									$("div#module_menu_items").fadeIn("fast");
-								else $("div#module_menu_top").removeClass("hover"),
-									$("div#module_menu_items").fadeOut("fast");
-								});
-						   		$("div#module_menu").hover( function() {}, function() {$("div#module_menu_items").fadeOut("fast"),$("div#module_menu_top").removeClass("hover","fast");});
-								$("div#module_menu_items li").hover( 
-																	function(){$(this).addClass("hover","fast"); return false;},
-																	function(){$(this).removeClass("hover", "normal");return false;}
-																	);
-						   });
+    $("#module_menu_top").click(function() {
+        if($("div#module_menu_items").is(":hidden"))
+            $("div#module_menu_top").addClass("hover"),
+            $("div#module_menu_items").fadeIn("fast");
+        else $("div#module_menu_top").removeClass("hover"),
+            $("div#module_menu_items").fadeOut("fast");
+    });
+    $("div#module_menu").hover( function() {}, function() {
+        $("div#module_menu_items").fadeOut("fast"),$("div#module_menu_top").removeClass("hover","fast");
+    });
+    $("div#module_menu_items li").hover(
+        function(){
+            $(this).addClass("hover","fast"); return false;
+        },
+        function(){
+            $(this).removeClass("hover", "normal");return false;
+        }
+        );
+});
