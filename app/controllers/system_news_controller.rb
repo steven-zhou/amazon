0ae218f9bdcca496d92f59f9209af1329feb0ab2
Wei <wei@powernet.com.au>
2009-11-05 12:13:09 +1100

    some changes to system news

--- app/controllers/system_news_controller.rb
+++ app/controllers/system_news_controller.rb
@@ -1,7 +1,13 @@
 class SystemNewsController < ApplicationController
 
+  def index
+    @active_system_news = SystemNews.find(:all, :conditions => ["status = ?", true], :order => "name")
+    @inactive_system_news = SystemNews.find(:all, :conditions => ["status = ?", false], :order => "name")
+  end
+
   def create
     @system_news = SystemNews.new(params[:system_news])
+    @system_news.status = true
     @system_news.save
     respond_to do |format|
       format.js
