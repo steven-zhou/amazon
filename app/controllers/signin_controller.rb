2104d235da0d57be7416ad0b88e2db5d3c2546d0
Anthony Winning <anthony@powernet.com.au>
2009-11-05 13:00:46 +1100

    Changed the styling for system messages.

--- app/controllers/signin_controller.rb
+++ app/controllers/signin_controller.rb
@@ -58,13 +58,13 @@ class SigninController < ApplicationController
         #flash.now[:warning] = flash_message(:type => "login_error")
         #rescue rescue_message = "your group do not have permissions"
         if login_account.nil?
-          rescue_message = "Login Account is error"
+          rescue_message = flash_message(:type => "login_error")
         else if  @group_types.nil?
-            rescue_message = "you do not have group"
+            rescue_message = flash_message(:type => "login_group_error")
           else if   @system_permission_types.nil?
-              rescue_message = "group permission is error"
+              rescue_message = flash_message(:type => "login_permission_error")
             else if @access_attempts_count.blank?
-                rescue_message = "your account has been locked, please call admin"
+                rescue_message = flash_message(:type => "login_count_error")
               end
             end
           end
@@ -207,71 +207,71 @@ class SigninController < ApplicationController
   private
   
   def login_as_user
-  begin
-        login_account = LoginAccount.authenticate(params[:user_name], params[:password])
-        session[:user] = login_account.id   # This will throw an exception if we do not have a valid login_account due to log in failing
-        @group_types = LoginAccount.validate_group(session[:user])
-        @system_permission_types = LoginAccount.validate_permission(session[:user])
-        @access_attempts_count = LoginAccount.validate_attempts_count(session[:user])
-        login_account.update_password = false
-        login_account.update_attributes(:last_ip_address => request.remote_ip, :last_login => Time.now())
-        session[:login_account_info] = login_account
+    begin
+      login_account = LoginAccount.authenticate(params[:user_name], params[:password])
+      session[:user] = login_account.id   # This will throw an exception if we do not have a valid login_account due to log in failing
+      @group_types = LoginAccount.validate_group(session[:user])
+      @system_permission_types = LoginAccount.validate_permission(session[:user])
+      @access_attempts_count = LoginAccount.validate_attempts_count(session[:user])
+      login_account.update_password = false
+      login_account.update_attributes(:last_ip_address => request.remote_ip, :last_login => Time.now())
+      session[:login_account_info] = login_account
 
-        #clear temp list data
-        @temp_list = TempList.find_by_login_account_id(session[:user])
-        @temp_list.destroy unless @temp_list.nil?
-        #create a temp list for all people on the lists(group lists and user lists)
-        @temp_list = TempList.new(:name => "List of all people", :list_size => 0, :last_date_generated => Date.today(), :status => true, :source => "Temp List", :source_type => "T", :allow_duplication => false, :login_account_id => session[:user])
-        @temp_list.save
-        temp_list_id = @temp_list.id
-        person_ids = Array.new
+      #clear temp list data
+      @temp_list = TempList.find_by_login_account_id(session[:user])
+      @temp_list.destroy unless @temp_list.nil?
+      #create a temp list for all people on the lists(group lists and user lists)
+      @temp_list = TempList.new(:name => "List of all people", :list_size => 0, :last_date_generated => Date.today(), :status => true, :source => "Temp List", :source_type => "T", :allow_duplication => false, :login_account_id => session[:user])
+      @temp_list.save
+      temp_list_id = @temp_list.id
+      person_ids = Array.new
 
-        #people on group list
-        LoginAccount.find(session[:user]).list_headers.each do |i|
-          @list_header = ListHeader.find(i)
-          @list_details = @list_header.list_details
-          @list_details.each do |list_detail|
-            person_ids << list_detail.person_id unless person_ids.include?(list_detail.person_id)
-          end
+      #people on group list
+      LoginAccount.find(session[:user]).list_headers.each do |i|
+        @list_header = ListHeader.find(i)
+        @list_details = @list_header.list_details
+        @list_details.each do |list_detail|
+          person_ids << list_detail.person_id unless person_ids.include?(list_detail.person_id)
         end
+      end
 
-        #people on user list
-        LoginAccount.find(session[:user]).user_lists.each do |i|
-          @list_header = ListHeader.find(i.list_header_id)
-          @list_details = @list_header.list_details
-          @list_details.each do |list_detail|
-            person_ids << list_detail.person_id unless person_ids.include?(list_detail.person_id)
-          end
+      #people on user list
+      LoginAccount.find(session[:user]).user_lists.each do |i|
+        @list_header = ListHeader.find(i.list_header_id)
+        @list_details = @list_header.list_details
+        @list_details.each do |list_detail|
+          person_ids << list_detail.person_id unless person_ids.include?(list_detail.person_id)
         end
+      end
 
-        person_ids.each do |i|
-          @list_detail = ListDetail.new(:list_header_id => temp_list_id, :person_id => i)
-          @list_detail.save
-        end
+      person_ids.each do |i|
+        @list_detail = ListDetail.new(:list_header_id => temp_list_id, :person_id => i)
+        @list_detail.save
+      end
 
 
-        redirect_to welcome_url
-      rescue
-        # If we threw an exception for not logging
-        #  in ok we will send a warning to the end user
-        #flash.now[:warning] = flash_message(:type => "login_error")
-        #rescue rescue_message = "your group do not have permissions"
-        if login_account.nil?
-          rescue_message = "Login Account is error"
-        else if  @group_types.nil?
-            rescue_message = "you do not have group"
-          else if   @system_permission_types.nil?
-              rescue_message = "group permission is error"
-            else if @access_attempts_count.blank?
-                rescue_message = "your account has been locked, please call admin"
-              end
+      redirect_to welcome_url
+    rescue
+      # If we threw an exception for not logging
+      #  in ok we will send a warning to the end user
+      #flash.now[:warning] = flash_message(:type => "login_error")
+      #rescue rescue_message = "your group do not have permissions"
+      if login_account.nil?
+        rescue_message = "Login Account is error"
+      else if  @group_types.nil?
+          rescue_message = "you do not have group"
+        else if   @system_permission_types.nil?
+            rescue_message = "group permission is error"
+          else if @access_attempts_count.blank?
+              rescue_message = "your account has been locked, please call admin"
             end
           end
         end
-
-        flash.now[:warning] = rescue_message
-
       end
+
+      flash.now[:warning] = rescue_message
+
+    end
   end
 
   def login_as_super_admin
