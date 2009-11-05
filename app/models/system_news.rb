2edf197c40960e8430077b84bf2f6581aee5109b
Wei <wei@powernet.com.au>
2009-11-05 17:45:22 +1100

    fix bug

--- app/models/system_news.rb
+++ app/models/system_news.rb
@@ -15,7 +15,7 @@ class SystemNews < ActiveRecord::Base
   end
 
   def self.last_three
-    active_number = SystemNews.find_all_by_status(true).size
+    active_number = SystemNews.find(:all, :conditions => ["status = ?", true]).size
     offset_left = active_number % 3
     offset_number = active_number / 3
     SystemNews.find(:all, :conditions => ["status = ?", true], :order => "created_at DESC", :offset => (offset_left == 0) ? (offset_number-1)*3 : offset_number*3, :limit => 3)
