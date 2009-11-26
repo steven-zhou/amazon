8cde224938e4f6a45bd1ca6cd5cfd958dba0b603
Anthony Winning <anthony@powernet.com.au>
2009-11-26 13:45:41 +1100

    Fixed a model error.

--- app/models/source.rb
+++ app/models/source.rb
@@ -2,7 +2,7 @@ class Source < ActiveRecord::Base
 
   belongs_to :campaign
 
-  validates_presence_of :campaign_id
+  validates_presence_of :campaign_id, :name
   validates_uniqueness_of :name, :scope => [:campaign_id]
 
 
