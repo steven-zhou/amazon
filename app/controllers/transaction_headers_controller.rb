51216dfb008e0c26061bf6b3a316ea675fe55a2b
lei <Lei@.powernet.com.au>
2009-12-07 11:40:39 +1000

    controller for transaction

--- app/controllers/transaction_headers_controller.rb
+++ app/controllers/transaction_headers_controller.rb
@@ -1,24 +1,5 @@
-class TransactionsController < ApplicationController
+class TransactionHeadersController < ApplicationController
   # System Log stuff added
-
-  def personal_transaction
-    @group_types = LoginAccount.find(session[:user]).group_types
-    @list_headers = @current_user.all_lists
-    @list_header = ListHeader.find(session[:current_list_id])
-    @p = @list_header.people_on_list
-    @person = Person.find(session[:current_person_id])
-    respond_to do |format|
-      format.html
-    end
-  end
-
-  def organisational_transaction    
-    @organisation = Organisation.find(session[:current_organisation_id])
-    @o = Organisation.find(:all, :order => "id")
-    respond_to do |format|
-      format.html
-    end
-  end
 
   def new
     @system_date = session[:clocktime].strftime("%d-%m-%Y")
@@ -51,10 +32,41 @@ class TransactionsController < ApplicationController
         flash.now[:error] = "Please Enter All Required Data"
       elsif(!@transaction_header.errors[:received_via_id].nil? && @transaction_header.errors.on(:received_via_id).include?("can't be blank"))
         flash.now[:error] = "Please Enter All Required Data"
-
+      else
+        flash.now[:error] = "Exception happen, please try again"
       end
-
+    end
+    respond_to do |format|
+      format.js
+    end
+  end
+  
+  def update
+    @transaction_header = TransactionHeader.find(params[:id])
+    if @transaction_header.update_attributes(params[:tansaction_header])
+      system_log("Login Account #{@current_user.user_name} (#{@current_user.id}) updated the details for TransactionHeader with ID #{@transaction_header.id}.")
+    else
+      system_log("Login Account #{@current_user.user_name} (#{@current_user.id}) had an error when attempting to update a transaction header record.")
+      #----------------------------presence - of------------------
+      if(!@transaction_header.errors[:transaction_date].nil? && @transaction_header.errors.on(:transaction_date).include?("can't be blank"))
+        flash.now[:error] = "Please Enter All Required Data"
+      elsif(!@transaction_header.errors[:bank_account_id].nil? && @transaction_header.errors.on(:bank_account_id).include?("can't be blank"))
+        flash.now[:error] = "Please Enter All Required Data"
+      elsif(!@transaction_header.errors[:receipt_meta_type_id].nil? && @transaction_header.errors.on(:receipt_meta_type_id).include?("can't be blank"))
+        flash.now[:error] = "Please Enter All Required Data"
+      elsif(!@transaction_header.errors[:receipt_type_id].nil? && @transaction_header.errors.on(:receipt_type_id).include?("can't be blank"))
+        flash.now[:error] = "Please Enter All Required Data"
+      elsif(!@transaction_header.errors[:received_via_id].nil? && @transaction_header.errors.on(:received_via_id).include?("can't be blank"))
+        flash.now[:error] = "Please Enter All Required Data"
+      else
+        flash.now[:error] = "Exception happen, please try again"
+      end
     end
   end
 
+
+
+
+
+  
 end
--- db/fixtures/contact_types.rb
+++ db/fixtures/contact_types.rb
@@ -16,3 +16,20 @@ ContactMetaType.create :name => "Home",   :tag_meta_type_id => phone.id,    :sta
 ContactMetaType.create :name => "Fax",   :tag_meta_type_id => phone.id,    :status => true
 ContactMetaType.create :name => "Business",   :tag_meta_type_id => website.id,    :status => true
 ContactMetaType.create :name => "Facebook",   :tag_meta_type_id => im.id,    :status => true
+
+
+puts "Initializing Receipt Types"
+ReceiptMetaMetaType.create :name => "Credit Card",    :status => true
+ReceiptMetaMetaType.create :name => "Cash",    :status => true
+ReceiptMetaMetaType.create :name => "Cheque",    :status => true
+
+
+credit_card = ReceiptMetaMetaType.find_by_name("Credit Card")
+cash = ReceiptMetaMetaType.find_by_name("Cash")
+cheque = ReceiptMetaMetaType.find_by_name("Cheque")
+
+
+ReceiptMetaType.create :name => "Visa Card",   :tag_meta_type_id => credit_card.id,    :status => true
+ReceiptMetaType.create :name => "Master Card",   :tag_meta_type_id => credit_card.id,    :status => true
+ReceiptMetaType.create :name => "Cash",   :tag_meta_type_id => cash.id,    :status => true
+
