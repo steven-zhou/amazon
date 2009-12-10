c9754dd01e7bf0b7c903665b7284d57e1fa2583a
Tao <tao@powernet.com.au>
2009-12-10 14:30:48 +1100

    fix

--- db/migrate/20091210032309_add_toberemoved_column_to_amazon_setting.rb
+++ db/migrate/20091210032309_add_toberemoved_column_to_amazon_setting.rb
@@ -1,9 +1,9 @@
 class AddToberemovedColumnToAmazonSetting < ActiveRecord::Migration
   def self.up
-    add_column :amazon_setting, :to_be_removed, :boolean
+    add_column :amazon_settings, :to_be_removed, :boolean
   end
 
   def self.down
-    remove_column :amazon_setting, :to_be_removed
+    remove_column :amazon_settings, :to_be_removed
   end
 end
