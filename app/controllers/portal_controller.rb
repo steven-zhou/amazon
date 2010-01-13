c32d86cbd62f5fd425151427e03ebed447a13b26
ubuntu <ubuntu@ubuntu-desktop.(none)>
2010-01-13 11:52:45 +1100

    finish add note after active potential member

--- app/controllers/portal_controller.rb
+++ app/controllers/portal_controller.rb
@@ -32,11 +32,22 @@ class PortalController < ApplicationController
   def active
     @potential_member = PotentialMember.find(params[:id].to_i) rescue @potential_member = PotentialMember.new
     if !@potential_member.new_record? && @potential_member.validation_key == params[:key]
+
+      #create a person record
       @person = Person.new(:first_name => @potential_member.first_name, :family_name => @potential_member.family_name)
       @person.save
+
+      #create an email record
       @email_type = ContactMetaMetaType.email.first
       @email = @person.emails.build(:value => @potential_member.email, :contact_meta_type_id => @email_type.contact_meta_types.first.id)
       @email.save
+
+      #create a note record
+      @note_type = NoteType.membership.first
+      @note = @person.notes.build(:label => "new_member", :short_description => "new member", :body_text => "new member", :active => true, :note_type_id => @note_type.id)
+      @note.save
+
+      #destroy temp potential member record
       @potential_member.destroy
       flash[:result] = "Your record is actived."
     else
