2571eb93cff39464adb645161d8a9b3cd02877c7
wei <wei@powernet.com.au>
2010-01-18 15:22:35 +1100

    add org export html

--- app/controllers/data_managers_controller.rb
+++ app/controllers/data_managers_controller.rb
@@ -22,30 +22,59 @@ class DataManagersController < ApplicationController
     @source_id = String.new
     @source_type = String.new
     if(params[:source]=="person")
-      if(params[:source_id].include?("query_"))
-        @source_id = params[:source_id].delete("query_")
-        @source_type = "query"
-        @query_header = QueryHeader.find(@source_id)
-        @people = @query_header.run
-      else
-        @source_id = params[:source_id].delete("list_")
-        @source_type = "list"
-        @people = ListHeader.find(@source_id).people_on_list
-      end
+      find_people_to_export
+      @entity_type = "person"
     else
-#      Export Organisations
+      # Export Organisations
+      find_organsiations_to_export
+      @entity_type = "organisation"
     end
 
     @file_name = params[:file_name].blank? ? "export" : params[:file_name]
     system_log("Login Account #{@current_user.user_name} (#{@current_user.id}) exported a report.")
 
     respond_to do |format|
-      format.html {render 'data_managers/export.html'}      
+      format.html {
+        if @entity_type == "person"
+          render 'data_managers/export.html'
+        else
+          render 'data_managers/org_export.html'
+        end
+      }
+
       format.xml {send_data((render 'data_managers/export.rxml'), :filename => "#{@file_name}.xml", :type => "text/xml")}
       format.csv {send_data((render 'data_managers/export.html'), :filename => "#{@file_name}.csv", :type => "text/csv")}
       format.pdf {pdf = PDF::Writer.new
-                  pdf = OutputPdf.generate_pdf(@source_type, @source_id, {}, {})
-                  send_data(pdf.render, :filename => "#{@file_name}.pdf", :type => "application/pdf")}
+        pdf = OutputPdf.generate_pdf(@source_type, @source_id, {}, {})
+        send_data(pdf.render, :filename => "#{@file_name}.pdf", :type => "application/pdf")}
+    end
+  end
+
+  private
+
+  def find_people_to_export
+    if(params[:source_id].include?("query_"))
+      @source_id = params[:source_id].delete("query_")
+      @source_type = "query"
+      @query_header = QueryHeader.find(@source_id)
+      @entities = @query_header.run
+    else
+      @source_id = params[:source_id].delete("list_")
+      @source_type = "list"
+      @entities = ListHeader.find(@source_id).entity_on_list
+    end
+  end
+
+  def find_organsiations_to_export
+    if(params[:source_id].include?("query_"))
+      @source_id = params[:source_id].delete("query_")
+      @source_type = "query"
+      @query_header = QueryHeader.find(@source_id)
+      @entities = @query_header.run
+    else
+      @source_id = params[:source_id].delete("list_")
+      @source_type = "list"
+      @entities = ListHeader.find(@source_id).entity_on_list
     end
   end
 
