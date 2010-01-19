ff19b88db9f1b57cb5976902fab1e127446478b5
wei <wei@powernet.com.au>
2010-01-19 16:38:10 +1100

    fix sorting problem for list.entity_on_list

--- app/models/organisation_list_header.rb
+++ app/models/organisation_list_header.rb
@@ -1,5 +1,5 @@
 class OrganisationListHeader < ListHeader
-  has_many :entity_on_list, :through => :list_details, :source => :organisation
+  has_many :entity_on_list, :through => :list_details, :source => :organisation, :order => "entity_id"
 
   def self.saved_lists
     OrganisationListHeader.find(:all, :order => "name")
