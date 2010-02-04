ea6ea9badfefa38adadf58b918e723566e7cee03
lei <Lei@.powernet.local>
2010-02-04 12:05:39 +1000

    finish the person profile

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -695,6 +695,21 @@ class PeopleController < ApplicationController
     end
   end
 
+  def page_initial
+    @render_page = params[:render_page]
+    @field = params[:field]
+    if params[:type]=="Person"
+      @type = "Person"
+      @person = Person.find_by_id(params[:params1])
+    else
+      @type = "Organisation"
+      @organisation = Organisation.find_by_id(params[:params1])
+    end
+    respond_to do |format|
+      format.js
+    end
+  end
+
 
 
 
