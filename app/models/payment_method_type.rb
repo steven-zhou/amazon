2aef6d7454d1dd58e71591da50038a626facce92
Wei <wei@powernet.com.au>
2010-03-02 12:48:51 +1100

    finish coding payment method

--- app/models/payment_method_type.rb
+++ app/models/payment_method_type.rb
@@ -4,6 +4,8 @@ class PaymentMethodType < TagType
   belongs_to :payment_method_meta_type, :class_name => "PaymentMethodMetaType", :foreign_key => "tag_meta_type_id"
   has_many :payment_methods
 
+  named_scope :manual, :conditions => {:tag_meta_type_id => PaymentMethodMetaType.find_by_name("Manual").id, :status => true, :to_be_removed => false}
+
 
   after_create :assign_priority
   before_destroy :reorder_priority
