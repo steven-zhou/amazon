8fb2e13a7923583db3de38182da551dfcb9b91b8
Lei <lei@ubuntu.(none)>
2010-03-03 16:43:36 +1100

    start membership fee

--- config/routes.rb
+++ config/routes.rb
@@ -174,6 +174,7 @@ ActionController::Routing::Routes.draw do |map|
 
 
   map.resources :tax_items
+  map.resources :membership_administrations, :collection => {:fee_items => :get}
 
 
   # The priority is based upon order of creation: first created -> highest priority.
