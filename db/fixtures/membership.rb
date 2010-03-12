64281f4c994f22951b029d86b39ba217955efe7e
Wei <wei@powernet.com.au>
2010-03-12 17:17:52 +1100

    fixture

--- db/fixtures/membership.rb
+++ db/fixtures/membership.rb
@@ -10,8 +10,13 @@ PersonMailTemplate.create :name=>"Membership Review Template", :body=>"<p>hi,</p
 <p>&nbsp;</p>
 <p>Amazon</p>" , :status => true,:to_be_removed =>false ,:template_category_id=> TemplateCategory.find_by_name('Membership').id
 
-PersonMailTemplate.create :name=>"Membership Finalize Template", :body=>"<p>hi,</p>
-<p>Your application has been&nbsp; finalized.</p>
+PersonMailTemplate.create :name=>"Membership Approve Template", :body=>"<p>hi,</p>
+<p>Your application has been&nbsp; approved.</p>
+<p>&nbsp;</p>
+<p>Amazon</p>" , :status => true,:to_be_removed =>false ,:template_category_id=> TemplateCategory.find_by_name('Membership').id
+
+PersonMailTemplate.create :name=>"Membership Reject Template", :body=>"<p>hi,</p>
+<p>Your application has been&nbsp; rejected.</p>
 <p>&nbsp;</p>
 <p>Amazon</p>" , :status => true,:to_be_removed =>false ,:template_category_id=> TemplateCategory.find_by_name('Membership').id
 
@@ -26,7 +31,12 @@ PersonEmailTemplate.create :name=>"Membership Review Email Template", :body=>"<p
 <p>&nbsp;</p>
 <p>Amazon</p>" , :status => true,:to_be_removed =>false ,:template_category_id=> TemplateCategory.find_by_name('Membership').id
 
-PersonEmailTemplate.create :name=>"Membership Finalize Email Template", :body=>"<p>hi,</p>
-<p>Your application has been&nbsp; finalized.</p>
+PersonEmailTemplate.create :name=>"Membership Approve Email Template", :body=>"<p>hi,</p>
+<p>Your application has been&nbsp; approved.</p>
+<p>&nbsp;</p>
+<p>Amazon</p>" , :status => true,:to_be_removed =>false ,:template_category_id=> TemplateCategory.find_by_name('Membership').id
+
+PersonEmailTemplate.create :name=>"Membership Reject Email Template", :body=>"<p>hi,</p>
+<p>Your application has been&nbsp; rejected.</p>
 <p>&nbsp;</p>
 <p>Amazon</p>" , :status => true,:to_be_removed =>false ,:template_category_id=> TemplateCategory.find_by_name('Membership').id
