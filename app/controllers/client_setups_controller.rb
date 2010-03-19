b94a67941306ef53900fa750949f142f686d869d
Wei <wei@powernet.com.au>
2010-03-19 15:05:19 +1100

    finish memberzone

--- app/controllers/client_setups_controller.rb
+++ app/controllers/client_setups_controller.rb
@@ -90,61 +90,61 @@ class ClientSetupsController < ApplicationController
     @client_setup = ClientSetup.first
 
 
-    check_installation_date = params[:client_setup][:installation_date].blank? ? true : valid_date(params[:client_setup][:halt_date])
-        check_halt_date = params[:client_setup][:installation_date].blank? ? true : valid_date(params[:client_setup][:halt_date])
-         check_birthday_date = params[:client_setup][:date_of_birth].blank? ? true : valid_date(params[:client_setup][:date_of_birth])
-       if check_installation_date&&check_halt_date&&check_birthday_date
-    if params[:parameters]
-      label_has_gap = nil
+    check_installation_date = params[:client_setup][:installation_date].blank? ? true : valid_date(params[:client_setup][:installation_date])
+    check_halt_date = params[:client_setup][:halt_date].blank? ? true : valid_date(params[:client_setup][:halt_date])
+    check_birthday_date = params[:client_setup][:date_of_birth].blank? ? true : valid_date(params[:client_setup][:date_of_birth])
+    if check_installation_date&&check_halt_date&&check_birthday_date
+      if params[:parameters]
+        label_has_gap = nil
 
-      i = 9
-      while i >= 0
-        if label_has_gap.nil?
-          # We've started moving from the bottom up
-          # We leave the value to be nil until we run into a filled value, when we become false
-          label_has_gap = params[:client_setup]["level_#{i}_label".to_sym].empty? ? nil : false
-        elsif !label_has_gap
-          # If we have not run into a gap so far, but have run into text
-          # If we run into some empty space we have a gap
-          label_has_gap = params[:client_setup]["level_#{i}_label".to_sym].empty? ? true : false
+        i = 9
+        while i >= 0
+          if label_has_gap.nil?
+            # We've started moving from the bottom up
+            # We leave the value to be nil until we run into a filled value, when we become false
+            label_has_gap = params[:client_setup]["level_#{i}_label".to_sym].empty? ? nil : false
+          elsif !label_has_gap
+            # If we have not run into a gap so far, but have run into text
+            # If we run into some empty space we have a gap
+            label_has_gap = params[:client_setup]["level_#{i}_label".to_sym].empty? ? true : false
+          end
+          i-=1
         end
-        i-=1
+
+        label_has_gap = false if label_has_gap.nil?
+
+        flash[:error] = "You Cannot Have A Gap Between Organisational Hierarchies." if label_has_gap
+
+
+        # Check to ensure each remark was filled in with a label
+        # Check there were no duplicate labels
+
+        labels = Hash.new
+
+        i = 0
+        while i < 9
+          flash[:error] = "A Remark Was Filled In Without A Label." if check_for_label_without_remark(params[:client_setup]["level_#{i}_label".to_sym], params[:client_setup]["level_#{i}_remarks".to_sym])
+          label = params[:client_setup]["level_#{i}_label".to_sym].downcase
+          labels["#{label}"] = labels["#{label}"].nil? ? 1 : (labels["#{label}"] + 1) unless label.empty?
+          i += 1
+        end
+
+        for key in labels.keys
+          flash[:error] = "You Have a Duplicate Label. All Labels Must Be Unique." if labels["#{key}"] > 1
+        end
       end
 
-      label_has_gap = false if label_has_gap.nil?
-
-      flash[:error] = "You Cannot Have A Gap Between Organisational Hierarchies." if label_has_gap
-
-
-      # Check to ensure each remark was filled in with a label
-      # Check there were no duplicate labels
-
-      labels = Hash.new
-
-      i = 0
-      while i < 9
-        flash[:error] = "A Remark Was Filled In Without A Label." if check_for_label_without_remark(params[:client_setup]["level_#{i}_label".to_sym], params[:client_setup]["level_#{i}_remarks".to_sym])
-        label = params[:client_setup]["level_#{i}_label".to_sym].downcase
-        labels["#{label}"] = labels["#{label}"].nil? ? 1 : (labels["#{label}"] + 1) unless label.empty?
-        i += 1
+      if (params[:password] && params[:password] != params[:repeat_password])
+        flash[:error] = "Passwords are not matched each other. Please try again."
+      elsif (@client_setup.update_attributes(params[:client_setup]) && flash[:error].nil?)
+        system_log("Login Account #{@current_user.user_name} (#{@current_user.id}) updated Client Setup with ID #{@client_setup.id}.")
+        flash[:message] = "Client Setup is updated"
       end
-
-      for key in labels.keys
-        flash[:error] = "You Have a Duplicate Label. All Labels Must Be Unique." if labels["#{key}"] > 1
-      end
-    end
-
-    if (params[:password] && params[:password] != params[:repeat_password])
-      flash[:error] = "Passwords are not matched each other. Please try again."
-    elsif (@client_setup.update_attributes(params[:client_setup]) && flash[:error].nil?)
-      system_log("Login Account #{@current_user.user_name} (#{@current_user.id}) updated Client Setup with ID #{@client_setup.id}.")
-      flash[:message] = "Client Setup is updated"
-    end
 
 
     else
       flash[:error] = "Please make sure the start date and end date are entered in valid format (dd-mm-yyyy)"
-      end
+    end
 
 
     if params[:installation]
@@ -174,34 +174,34 @@ class ClientSetupsController < ApplicationController
     system_log("Login Account #{@current_user.user_name} (#{@current_user.id}) searched System Log entries.")
 
 
-   @query=""
+    @query=""
 
     if (params[:start_date].blank? || params[:end_date].blank? )
-         params[:start_date]= '01-01-1909' if params[:start_date].blank?
-         params[:end_date] = '12-31-2599' if params[:end_date].blank?
+      params[:start_date]= '01-01-1909' if params[:start_date].blank?
+      params[:end_date] = '12-31-2599' if params[:end_date].blank?
          
-        if valid_date(params[:start_date]) && valid_date(params[:end_date])
-         @start_date=params[:start_date]
-         @end_date=params[:end_date]
-       else
-       flash.now[:error] = "Please make sure the start date and end date are entered in valid format (dd-mm-yyyy)"
-    end
+      if valid_date(params[:start_date]) && valid_date(params[:end_date])
+        @start_date=params[:start_date]
+        @end_date=params[:end_date]
+      else
+        flash.now[:error] = "Please make sure the start date and end date are entered in valid format (dd-mm-yyyy)"
+      end
     else
 
 
-    if valid_date(params[:start_date]) && valid_date(params[:end_date])
+      if valid_date(params[:start_date]) && valid_date(params[:end_date])
 
   
-    @start_date = ((!params[:start_date].nil? && !params[:start_date].empty?) ? params[:start_date].to_date.strftime('%Y-%m-%d') : '0001-01-01 00:00:01')
-    @end_date = ((!params[:end_date].nil? && !params[:end_date].empty?) ? params[:end_date].to_date.strftime('%Y-%m-%d') : '9999-12-31 23:59:59')
-    else
-       flash.now[:error] = "Please make sure the start date and end date are entered in valid format (dd-mm-yyyy)"
-    end
+        @start_date = ((!params[:start_date].nil? && !params[:start_date].empty?) ? params[:start_date].to_date.strftime('%Y-%m-%d') : '0001-01-01 00:00:01')
+        @end_date = ((!params[:end_date].nil? && !params[:end_date].empty?) ? params[:end_date].to_date.strftime('%Y-%m-%d') : '9999-12-31 23:59:59')
+      else
+        flash.now[:error] = "Please make sure the start date and end date are entered in valid format (dd-mm-yyyy)"
+      end
     
     end
 
     if flash.now[:error].nil?
-    @query << "start_date="+@start_date+"&end_date="+@end_date
+      @query << "start_date="+@start_date+"&end_date="+@end_date
     end
 
 
@@ -209,7 +209,7 @@ class ClientSetupsController < ApplicationController
     @user_name = ((!params[:user_name].nil? && !params[:user_name].empty?) ? params[:user_name] : '')
     unless @user_name.empty?
 
-        @query << "&user_name="+@user_name
+      @query << "&user_name="+@user_name
     end
 
     @status = ((!params[:status].nil? && !params[:status].empty?) ? params[:status] : '')
@@ -231,19 +231,19 @@ class ClientSetupsController < ApplicationController
 
     system_log("Login Account #{@current_user.user_name} (#{@current_user.id}) searched System Log entries for achiving.")
 
-        if (params[:start_date].blank? || params[:end_date].blank?)
-         start_date= '0001-01-01 00:00:01'
-         end_date = '9999-12-31 23:59:59'
+    if (params[:start_date].blank? || params[:end_date].blank?)
+      start_date= '0001-01-01 00:00:01'
+      end_date = '9999-12-31 23:59:59'
     else
 
 
-     if valid_date(params[:start_date]) && valid_date(params[:end_date])
-       start_date = ((!params[:start_date].nil? && !params[:start_date].empty?) ? params[:start_date].to_date.strftime('%Y-%m-%d') : '0001-01-01 00:00:01')
-       end_date = ((!params[:end_date].nil? && !params[:end_date].empty?) ? params[:end_date].to_date.strftime('%Y-%m-%d') : '9999-12-31 23:59:59')
-     else
+      if valid_date(params[:start_date]) && valid_date(params[:end_date])
+        start_date = ((!params[:start_date].nil? && !params[:start_date].empty?) ? params[:start_date].to_date.strftime('%Y-%m-%d') : '0001-01-01 00:00:01')
+        end_date = ((!params[:end_date].nil? && !params[:end_date].empty?) ? params[:end_date].to_date.strftime('%Y-%m-%d') : '9999-12-31 23:59:59')
+      else
         flash[:error] = "Please make sure the start date and end date are entered in valid format (dd-mm-yyyy)"
-     end
-        end
+      end
+    end
 
     user_name = ((!params[:archive_user_name].nil? && !params[:archive_user_name].empty?) ? params[:archive_user_name] : '%%')
     
@@ -274,7 +274,7 @@ class ClientSetupsController < ApplicationController
 
   end
 
-      def delete_archive_system_log_entries
+  def delete_archive_system_log_entries
    
 
     user_name = ((!params[:user_name].nil? && !params[:user_name].empty?) ? params[:user_name] : '%%')
@@ -295,7 +295,7 @@ class ClientSetupsController < ApplicationController
     redirect_to :action => "system_log_management"
 
 
-    end
+  end
 
 
 
@@ -503,7 +503,7 @@ class ClientSetupsController < ApplicationController
 
   def destroy_client_bank_account
     @client_bank_account = ClientBankAccount.find(params[:id])
-#    @client_bank_account.destroy
+    #    @client_bank_account.destroy
     @client_bank_account.to_be_removed = true
     @client_bank_account.save
     respond_to do |format|
@@ -511,9 +511,9 @@ class ClientSetupsController < ApplicationController
     end
   end
 
-    def retrieve_client_bank_account
+  def retrieve_client_bank_account
     @client_bank_account = ClientBankAccount.find(params[:id])
-#    @client_bank_account.destroy
+    #    @client_bank_account.destroy
     @client_bank_account.to_be_removed = false
     @client_bank_account.save
     respond_to do |format|
