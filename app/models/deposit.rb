8ce65c040c27fcc71f7eed2c1445151a29171248
matthew <matthew@hotmail.com>
2010-03-30 17:14:37 +1100

    get

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -23,7 +23,7 @@ class Deposit < ActiveRecord::Base
   #validates_format_of :business_date, :with => /^(((0?[1-9]|1[012])\-(0?[1-9]|1\d|2[0-8])|(0?[13456789]|1[012])\-(29|30)|(0?[13578]|1[02])\-31)\-(19|[2-9]\d)\d{2}|0?2\-29\-((19|[2-9]\d)(0[48]|[2468][048]|[13579][26])|(([2468][048]|[3579][26])00)))$/
 
   before_update :update_total_amount,:update_children_reciepts_bank_run_id
-
+  before_save :to_be_bank_validation
   private
   def self.first_record
     Deposit.first
