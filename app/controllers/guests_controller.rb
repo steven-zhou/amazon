925afd79ea8019bf89d5f6e1b20737735cff8a80
Lei <lei@ubuntu.(none)>
2010-04-12 14:25:29 +1000

    finish the model and controller

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,32 +1,47 @@
 class GuestsController < ApplicationController
 
   layout nil
-
   before_filter :check_authentication, :except => [:index,:new,:create,:login]
 
   def index
 
-
   end
 
+  #--comment when you click the register button for guest register process
   def new
-
-
-    
+    @guest = SystemUser.new
+    respond_to do |format|
+      format.js
+    end
   end
 
 
   def create
-   guest = Guest.new
+    guest = Guest.new
+    @guest = Guest.new(params[:guest])
+    #----comment use the password setter method , set it as system random generate password
+    @guest.password = Guest.generate_password
+    if guest.save
+      #do sth
+    else
 
-    guest.family_name = params[:family_name]
-    guest.first_name = params[:first_name]
-    guest.phone_num = params[:phone]
-    guest.email_address = params[:email]
-    guest.password = params[:new_password]
-    guest.save
+      #----------------------------presence - of------------------------#
+      if (!@guest.errors[:email].nil? && @guest.errors.on(:email).include?("can't be blank"))
+        flash.now[:error] = flash_message(:type => "field_missing", :field => "email")
+      elsif(!@guest.errors[:first_name].nil? && @guest.errors.on(:first_name).include?("can't be blank"))
+        flash.now[:error] = flash_message(:type => "field_missing", :field => "first_name")
+      elsif(!@guest.errors[:family_name].nil? && @guest.errors.on(:family_name).include?("can't be blank"))
+        flash.now[:error] = flash_message(:type => "field_missing", :field => "family_name")
+      elsif(!@guest.errors[:phone_num].nil? && @guest.errors.on(:phone_num).include?("can't be blank"))
+        flash.now[:error] = flash_message(:type => "field_missing", :field => "phone_num")
 
-
+        #-----------------------validate--presence of ------------------------
+      elsif(!@guest.errors[:email].nil? && @guest.errors.on(:email).include?("has already been taken"))
+        flash.now[:error] = flash_message(:type => "uniqueness_error", :field => "email") 
+      else
+        flash.now[:error] = flash_message(:message => "Please Check Your Input, There are some invalid input")
+      end
+    end
     render :action=>"index"
   end
 
@@ -34,12 +49,12 @@ class GuestsController < ApplicationController
 
   def login
 
-   if Guest.authenticate(params[:user_name],params[:password])
+    if Guest.authenticate(params[:user_name],params[:password])
      
-       render :action=>"index"
-   else
+      render :action=>"index"
+    else
       render "login.rhtml"
-   end
+    end
 
   end
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,15 +1,16 @@
 class Guest < ActiveRecord::Base
 
-
-
-
   validates_uniqueness_of :email, :case_sensitive => false
-  validates_presence_of  :email
+  validates_presence_of  :email, :first_name,:family_name,  :phone_num
+  validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "Invalid email"
+  validates_length_of :password, :within => 6..30
+  validates_format_of :password, :with => /^[A-Za-z0-9!@$%^&*()#]+$/i
 
+  default_scope :order => "id ASC"
 
+  #--comment----for check the guest input user_name and password is correct----
 
-
-    def self.authenticate(email, password)
+  def self.authenticate(email, password)
     guest = Guest.find(:first, :conditions => ['email = ?', email])
     if guest.nil?
       raise "Username or password invalid"
@@ -20,7 +21,7 @@ class Guest < ActiveRecord::Base
     end
   end
 
-
+   #--comment----for the user type password , this is password setter----
 
 
   def password=(pass)
@@ -32,6 +33,15 @@ class Guest < ActiveRecord::Base
     #    self.password_updated_at = Time.now()
     salt = [Array.new(6){rand(256).chr}.join].pack("m").chomp
     self.password_salt, self.password_hash = salt, Digest::SHA256.hexdigest(@password + salt)
+  end
+
+  #--comment----for system generate random password for the guest randomly----
+
+  def self.generate_password(length=6)
+    chars = 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ23456789'
+    password = ''
+    length.times { |i| password << chars[rand(chars.length)] }
+    password
   end
 
 end
